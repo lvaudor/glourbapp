31ca4f6bb02179ff1d0c9a8a66f39026cec2ca6b
lvaudor <lise.vaudor@ens-lyon.fr>
2023-08-24 11:23:35 +0200

    better parameterization OSM

--- R/mod_WP3.R
+++ R/mod_WP3.R
@@ -16,7 +16,7 @@ mod_WP3_ui <- function(id){
                          "Choose city",
                          choices=cities,
                          selected="Ahmedabad"),
-             selectInput(ns("group"),
+             checkboxGroupInput(ns("group"),
                          "Choose type",
                          choices=unique(map_elems_global$group)),
              plotOutput(ns("plot_osmglobal"))
@@ -41,12 +41,11 @@ mod_WP3_server <- function(id){
         paste0("mapinfo/mapinfo_",input$city,".RDS"),
         package="glourbapp")) %>%
         dplyr::filter(nelems>0)
-      mapinfo=mapinfo %>%
-        dplyr::filter(group==input$group)
       }
     )
     output$plot_osmglobal=renderPlot({
-      mapinfo=get_mapinfo()
+      mapinfo=get_mapinfo() %>%
+        dplyr::filter(group %in% input$group)
       colorscale=mapinfo %>%
         dplyr::select(key,value,color) %>%
         unique()
@@ -55,14 +54,13 @@ mod_WP3_server <- function(id){
       ggplot2::ggplot(mapinfo,
                       ggplot2::aes(x=value,y=nelems,fill=value))+
         ggplot2::geom_bar(stat="identity",color="dark grey")+
-        #ggplot2::geom_point(ggplot2::aes(y=nelems_moy), pch="|", size=2)+
         ggplot2::coord_flip()+
         ggplot2::scale_fill_manual(values=colorscale_vec)+
         ggplot2::theme(legend.position="none")+
-        ggplot2::scale_y_sqrt()
+        ggplot2::scale_y_sqrt()+
+        ggplot2::facet_wrap(facets=ggplot2::vars(group))
     })
     output$map_city=leaflet::renderLeaflet({
-      mapinfo=get_mapinfo()
       shape=  readRDS(system.file(
                         paste0("shapes/shape_",input$city,".RDS"),
                         package="glourbapp"))
@@ -82,12 +80,21 @@ mod_WP3_server <- function(id){
     observe({
       mapinfo=get_mapinfo()
       mymap=leaflet::leafletProxy(ns("map_city"))
-      for (i in 1:nrow(mapinfo)){mymap=mymap %>%
+      if(!is.null(input$group)){
+      for (i in 1:nrow(mapinfo)){
+        if(mapinfo$group[i] %in% input$group){
+          mymap=mymap %>%
           add_to_map(mapinfo$osmdata[i][[1]],
                      color=mapinfo$color[i],
-                     layergroup=mapinfo$group[i])
+                     layergroup=mapinfo$value[i]
+                    )
+        }
+        if(!(mapinfo$group[i] %in% input$group)){
+        mymap=mymap %>%
+          leaflet::clearGroup(mapinfo$value[i])
+        }
+      }# end for loop
       }
-
     })
   })
 }
